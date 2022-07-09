3eaee61732afa711867a61b3cd58ace07f8e38a2
Ksenia Dubrovskaya <108101241+KsuLip2022@users.noreply.github.com>
2022-07-09 16:00:35 -0400

    Add files via upload

---
+++ Javaloop.java
@@ -0,0 +1,40 @@
+
+public class Javaloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          //   for (int i = 10; i < 20; i ++) {
+           	//		System.out.println("Value of i = " + i);
+//	}
+		
+		         int j = 2;
+		       do {
+                   System.out.println("Value of j is " + j);
+		           j++;
+             }	
+		       while (j>=8);
+          //       	
+             
+	//	int i = 0;
+	//	do {
+	//	  System.out.println(i);
+	//	  i++;
+	//	}
+	//	while (i < 5);
+	
+		
+		
+		
+                
+              //  int i = 0;
+              //  while (i < 5) {
+             //     System.out.println(i);
+             //     i++;
+             //   }  
+                
+                
+                
+                
+                
+}
+}
